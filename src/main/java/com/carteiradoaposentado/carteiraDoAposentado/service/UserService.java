9cdd51d8d75ccfe10c2e24bdb1b0a20d1a08e8dc
mayke.furtado <mayke.furtado@arcom.com.br>
2021-10-22 09:43:33 -0300

    feat: buscando usuario por id

--- src/main/java/com/carteiradoaposentado/carteiraDoAposentado/service/UserService.java
+++ src/main/java/com/carteiradoaposentado/carteiraDoAposentado/service/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -16,5 +17,10 @@ public class UserService {
     public List<User> findAll(){
 
         return userRepository.findAll();
+    }
+
+    public Optional<User> findById(final String id){
+
+        return userRepository.findById(id);
     }
 }
---
+++ src/main/java/com/carteiradoaposentado/carteiraDoAposentado/service/exception/NotFoundException.java
@@ -0,0 +1,8 @@
+package com.carteiradoaposentado.carteiraDoAposentado.service.exception;
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(final String msg){
+        super(msg);
+    }
+}
