e07c1aba1ce0fcc482e7e58c8c6ba06debfca3ad
mayke.furtado <mayke.furtado@arcom.com.br>
2021-12-13 12:51:16 -0300

    refactor: rafactor checkStyle

--- src/main/java/br/com/carteiradoaposentado/CarteiraDoAposentadoApplication.java
+++ src/main/java/br/com/carteiradoaposentado/CarteiraDoAposentadoApplication.java
@@ -3,14 +3,10 @@ package br.com.carteiradoaposentado;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.annotation.PostConstruct;
-import java.util.TimeZone;
-
 @SpringBootApplication
 public class CarteiraDoAposentadoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarteiraDoAposentadoApplication.class, args);
 	}
-
 }
--- src/main/java/br/com/carteiradoaposentado/commons/constantes/Setor.java
+++ src/main/java/br/com/carteiradoaposentado/commons/constantes/Setor.java
@@ -7,74 +7,72 @@ public enum Setor implements Constante<Short> {
     /**
      * Tecnologia
      */
-    TECNOLOGIA( "Tecnologia", (short) 1 ),
+    TECNOLOGIA("Tecnologia", (short) 1),
 
     /**
      * Consumo
      */
-    CONSUMO( "Consumo", (short) 2 ),
+    CONSUMO("Consumo", (short) 2),
 
     /**
      * Consumo cíclico
      */
-    CONSUMO_CICLICO( "Consumo cíclico", (short) 3 ),
+    CONSUMO_CICLICO("Consumo cíclico", (short) 3),
 
     /**
      * Financeiro
      */
-    FINANCEIRO( "Financeiro", (short) 4 ),
+    FINANCEIRO("Financeiro", (short) 4),
 
     /**
      * Saúde
      */
-    SAUDE( "Saúde", (short) 5 ),
+    SAUDE("Saúde", (short) 5),
 
     /**
      * Materiais Básicos
      */
-    MATERIAIS_BASICOS( "Materiais Básicos", (short) 6 ),
+    MATERIAIS_BASICOS("Materiais Básicos", (short) 6),
 
     /**
      * Utilidade pública
      */
-    UTILIDADE_PUBLICA( "Utilidade pública", (short) 7 ),
+    UTILIDADE_PUBLICA("Utilidade pública", (short) 7),
 
     /**
      * Shoppings
      */
-    FII_SHOPPINGS( "FII Shoppings", (short) 8 ),
+    FII_SHOPPINGS("FII Shoppings", (short) 8),
 
     /**
      * Lajes Corporativas
      */
-    FII_LAJES_CORPORATIVAS( "FII Lajes Corporativas", (short) 9 ),
+    FII_LAJES_CORPORATIVAS("FII Lajes Corporativas", (short) 9),
 
     /**
      * Galpões Industriais
      */
-    FII_GALPOES( "FII Galpões Industriais", (short) 10 ),
+    FII_GALPOES("FII Galpões Industriais", (short) 10),
 
     /**
      * Hotéis
      */
-    FII_HOTEIS( "FII Hotéis", (short) 11 ),
+    FII_HOTEIS("FII Hotéis", (short) 11),
 
     /**
      * Fundos
      */
-    FII_FUNDOS( "FII Fundos", (short) 12 ),
+    FII_FUNDOS("FII Fundos", (short) 12),
 
     /**
      * Papel
      */
-    FII_PAPEL( "FII Papel", (short) 13 ),
+    FII_PAPEL("FII Papel", (short) 13),
 
     /**
      * Outros
      */
-    OUTROS( "Outros", (short) 14 )
-
-    ;
+    OUTROS("Outros", (short) 14);
 
     private final String descricao;
     private final Short valor;
