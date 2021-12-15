3359616e869a2feb9e7687625304a39e02389146
mayke.furtado <mayke.furtado@arcom.com.br>
2021-12-15 13:52:38 -0300

    feat: criando test fundamento

---
+++ src/test/java/br/com/carteiradoaposentado/FundamentoTest.java
@@ -0,0 +1,18 @@
+package br.com.carteiradoaposentado;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class FundamentoTest {
+
+    @Test
+    @Order(1)
+    void cadastro() {
+
+    }
+}
