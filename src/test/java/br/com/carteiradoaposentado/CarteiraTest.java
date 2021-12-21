3a19386943a27b41ee62c9448193f2d21e04da6a
mayke.furtado <mayke.furtado@arcom.com.br>
2021-12-21 17:12:44 -0300

    feat: cobertura de teste carteiraService

--- src/test/java/br/com/carteiradoaposentado/CarteiraTest.java
+++ src/test/java/br/com/carteiradoaposentado/CarteiraTest.java
@@ -1,11 +1,18 @@
 package br.com.carteiradoaposentado;
 
+import br.com.carteiradoaposentado.commons.constantes.Categoria;
+import br.com.carteiradoaposentado.commons.constantes.Setor;
+import br.com.carteiradoaposentado.commons.constantes.Tipo;
+import br.com.carteiradoaposentado.commons.dto.CarteiraDto;
+import br.com.carteiradoaposentado.domain.Carteira;
 import br.com.carteiradoaposentado.service.CarteiraService;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
 
 @SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -33,18 +40,37 @@ public class CarteiraTest {
     }
 
     @Test
+    @Order(4)
     void criarNovaCarteira() {
+        Set<Carteira.PorcentagemTipo> tipos = new HashSet<>();
+        tipos.add(new Carteira.PorcentagemTipo(Tipo.ACAO, new BigDecimal("10")));
+        Set<Carteira.PorcentagemCategoria> categorias = new HashSet<>();
+        categorias.add(new Carteira.PorcentagemCategoria(Categoria.BY_ROAD, new BigDecimal("10")));
+        Set<Carteira.PorcentagemSetor> setores = new HashSet<>();
+        setores.add(new Carteira.PorcentagemSetor(Setor.FII_FUNDOS, new BigDecimal("10")));
+        Set<Carteira.TipoQtds> qtds = new HashSet<>();
+        qtds.add(new Carteira.TipoQtds(Tipo.FII, 12L));
 
+        CarteiraDto dto = new CarteiraDto(tipos, categorias, setores, qtds);
+        Assertions.assertNotNull(carteiraService.create("4", dto));
     }
 
     @Test
+    @Order(5)
     void criarNovaCarteiraParaUsuarioQueJaTem() {
+        Set<Carteira.PorcentagemTipo> tipos = new HashSet<>();
+        tipos.add(new Carteira.PorcentagemTipo(Tipo.ACAO, new BigDecimal("10")));
+        Set<Carteira.PorcentagemCategoria> categorias = new HashSet<>();
+        categorias.add(new Carteira.PorcentagemCategoria(Categoria.BY_ROAD, new BigDecimal("10")));
+        Set<Carteira.PorcentagemSetor> setores = new HashSet<>();
+        setores.add(new Carteira.PorcentagemSetor(Setor.FII_FUNDOS, new BigDecimal("10")));
+        Set<Carteira.TipoQtds> qtds = new HashSet<>();
+        qtds.add(new Carteira.TipoQtds(Tipo.FII, 12L));
 
+        final Carteira carteiraAtual = carteiraService.buscarPorIdUsuario("4");
+        CarteiraDto dto = new CarteiraDto(tipos, categorias, setores, qtds);
+        final Carteira carteira = carteiraService.create("4", dto);
+        Assertions.assertEquals(carteiraAtual.getId(), carteira.getId());
     }
 
-    @Test
-    void atualizarCarteira() {
-
-    }
-    
 }
