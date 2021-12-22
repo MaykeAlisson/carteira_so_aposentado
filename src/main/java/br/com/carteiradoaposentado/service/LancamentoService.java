0356c4038f36f0d0bdabe7c5eba3af24924b686e
mayke.furtado <mayke.furtado@arcom.com.br>
2021-12-22 17:38:50 -0300

    feat: crud lancamento

--- src/main/java/br/com/carteiradoaposentado/service/LancamentoService.java
+++ src/main/java/br/com/carteiradoaposentado/service/LancamentoService.java
@@ -1,4 +1,9 @@
 package br.com.carteiradoaposentado.service;
 
+import br.com.carteiradoaposentado.commons.dto.LancamentoDto;
+import br.com.carteiradoaposentado.domain.Lancamento;
+
 public interface LancamentoService {
+
+    Lancamento create(String idUser, LancamentoDto dto);
 }
