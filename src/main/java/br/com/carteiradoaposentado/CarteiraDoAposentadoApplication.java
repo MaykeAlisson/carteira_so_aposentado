9c9116122b73f602fa00f6b18aa800088f3f8966
mayke.furtado <mayke.furtado@arcom.com.br>
2021-12-30 08:58:54 -0300

    refactor: CheckTyle

--- src/main/java/br/com/carteiradoaposentado/CarteiraDoAposentadoApplication.java
+++ src/main/java/br/com/carteiradoaposentado/CarteiraDoAposentadoApplication.java
@@ -7,7 +7,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableSwagger2
 public class CarteiraDoAposentadoApplication {
-    public static void main(String[] args) {
-      SpringApplication.run(CarteiraDoAposentadoApplication.class, args);
-    }
+ public static void main(String[] args) {
+   SpringApplication.run(CarteiraDoAposentadoApplication.class, args);
+ }
 }
